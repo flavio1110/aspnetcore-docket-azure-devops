a08f12ff5bbf91737ea5a07e8eec786b446146f5
Flavio Silva <flavio1110@gmail.com>
2018-09-24 20:12:06 +0200

    add integration tests

---
+++ netcore-api-docker.Api.IntegrationTests/ValuesControllerTest.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+using netcore_api_docker;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Microsoft.AspNetCore;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+
+namespace netcore_api_docker.Api.IntegrationTests
+{
+    public class ValuesControllerTest
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public ValuesControllerTest()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseEnvironment("Development"));
+            _client = _server.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task GetValues_Returns200()
+        {
+            var response = await _client.GetAsync("/api/values");
+ 
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
